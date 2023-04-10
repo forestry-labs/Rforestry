f4898485bc16a51c86d06309f5a1933f537d223d
Boban Petrovic <petrovicboban.g@googlemail.com>
2023-04-10 19:05:02 +0000

    No need to install R package inside test script any more

--- cross_language_test/run.sh
+++ cross_language_test/run.sh
@@ -4,10 +4,6 @@ echo "=== Running Python script started  ==="
 python3 python_test.py > /tmp/python_output
 echo "=== Running Python script finished ==="
 echo
-echo "=== Build and install R package    ==="
-R CMD build ../R
-PACKAGE=$(ls -1 *.tar.gz) R -e "install.packages(Sys.getenv('PACKAGE'), repos=NULL, type='source')"
-echo "=== R package installed            ==="
 echo
 echo "=== Running R script started       ==="
 Rscript R_test.R > /tmp/R_output
