1784ebf732937550e15195fd711a85c1bd7c0a44
edwardwliu <edwardliu.w@gmail.com>
2023-04-22 13:42:05 -0400

    cast int to float for dict keys

--- Python/random_forestry/preprocessing.py
+++ Python/random_forestry/preprocessing.py
@@ -58,13 +58,14 @@ def find_match(arr_a: Union[np.ndarray, List], arr_b: Union[np.ndarray, List]) -
     """
 
     temp_dict = {}
-    print(type(arr_b))
 
     for index, element in enumerate(arr_b):
+        if isinstance(element, int):
+            element = float(element)
         if str(element) not in temp_dict:
             temp_dict[str(arr_b[index])] = index
 
-    return np.array([temp_dict[str(val)] for val in arr_a])
+    return np.array([temp_dict[str(float(val)) if isinstance(val, int) else str(val)] for val in arr_a])
 
 
 def forest_checker(forest) -> None:
@@ -190,7 +191,6 @@ def preprocess_testing(x, categorical_feature_cols: np.ndarray, categorical_feat
 
     # For each categorical feature, encode x into numeric representation
     for categorical_feature_mapping_ in categorical_feature_mapping:
-        print(categorical_feature_mapping_)
         categorical_feature_col = categorical_feature_mapping_["categoricalFeatureCol"]
         # Get all unique feature values
         testing_unique_feature_values = x.iloc[:, categorical_feature_col].unique()
@@ -209,7 +209,6 @@ def preprocess_testing(x, categorical_feature_cols: np.ndarray, categorical_feat
             categorical_feature_mapping_["uniqueFeatureValues"] = unique_feature_values
             categorical_feature_mapping_["numericFeatureValues"] = numeric_feature_values
 
-        print(unique_feature_values)
         x.iloc[:, categorical_feature_col] = pd.Series(
             find_match(x.iloc[:, categorical_feature_col], unique_feature_values),
             dtype="category",
