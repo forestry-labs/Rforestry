144a55ab5c5e585c6d788400e82eaee127f91670
Ilia Shutov <ilia.shutov@bwater.com>
2023-08-11 09:04:16 +0200

    R test rewritten, using jsonlite R library

--- R/tests/testthat/test-export_treelite_json.R
+++ R/tests/testthat/test-export_treelite_json.R
@@ -1,6 +1,10 @@
-library(rjson)
+library(jsonlite)
 test_that("Tests that exporting RF to Treelite JSON works", {
   context("Export RF to Treelite JSON")
+
+  check_in_range <- function(x, lower, upper) {
+    all(x >= lower & x <= upper)
+  }
 
   seed <- 238943202
   set.seed(seed)
@@ -10,18 +14,23 @@ test_that("Tests that exporting RF to Treelite JSON works", {
   x_train <- iris[-test_idx, -1]
   y_train <- iris[-test_idx, 1]
   rf <- forestry(x = x_train, y = y_train, ntree = 1, maxDepth = 1, seed = seed)
+  export_treelite_json_ret <- export_treelite_json(rf)
+  json_parsed <- fromJSON(export_treelite_json_ret)
 
-  export_treelite_json_result <- export_treelite_json(rf)
-  result_json_obj <- fromJSON(json_str = export_treelite_json_result)
-  json_data_frame <- as.data.frame(result_json_obj)
+  json_parsed$num_feature == 4
+  json_parsed$task_type == "kBinaryClfRegr"
 
-  testthat::expect_equivalent(json_data_frame["num_feature"],                    4)
-  testthat::expect_equivalent(json_data_frame["task_type"],                      "kBinaryClfRegr")
-  testthat::expect_equivalent(json_data_frame["trees.root_id"],                  3)
-  testthat::expect_equivalent(json_data_frame["trees.nodes.split_feature_id"],   0)
-  testthat::expect_equivalent(json_data_frame["trees.nodes.left_child"],         1)
-  testthat::expect_equivalent(json_data_frame["trees.nodes.right_child"],        2)
-  testthat::expect_equivalent(json_data_frame["trees.nodes.left_child"],         1)
-  testthat::expect_equivalent(json_data_frame["trees.nodes.node_id.1"],          1)
-  testthat::expect_equivalent(json_data_frame["trees.nodes.node_id.2"],          2)
+  check_in_range(json_parsed$trees$root_id, 1, 3)
+
+  df <- as.data.frame(json_parsed$trees$nodes)
+  check_in_range(df["node_id"], 1, 3)
+
+  feature_ids <- df[["split_feature_id"]]
+  check_in_range(feature_ids[1], 0, 3) && all(is.na(feature_ids[2:length(feature_ids)]))
+
+  left_child <- df[["left_child"]]
+  check_in_range(left_child[1], 1, 3) && all(is.na(left_child[2:length(left_child)]))
+
+  right_child <- df[["right_child"]]
+  check_in_range(right_child[1], 1, 3) && all(is.na(right_child[2:length(right_child)]))
 })
