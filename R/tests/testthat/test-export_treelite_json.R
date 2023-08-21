5bd35cdde357211e1e3258058c2e3b1ed4103dfc
Ilia Shutov <ilia.shutov@bwater.com>
2023-08-21 09:24:24 +0200

    R export_json test fix and update

--- R/tests/testthat/test-export_treelite_json.R
+++ R/tests/testthat/test-export_treelite_json.R
@@ -3,7 +3,7 @@ test_that("Tests that exporting RF to Treelite JSON works", {
   context("Export RF to Treelite JSON")
 
   check_in_range <- function(x, lower, upper) {
-    all(x >= lower & x <= upper)
+    expect_true(all(x >= lower & x <= upper))
   }
 
   seed <- 238943202
@@ -16,6 +16,22 @@ test_that("Tests that exporting RF to Treelite JSON works", {
   rf <- forestry(x = x_train, y = y_train, ntree = 1, maxDepth = 1, seed = seed)
   export_json_ret <- export_json(rf)
   json_parsed <- fromJSON(export_json_ret)
+
+  node_exists_with_nonzero <- function(node_key_name) {
+    df <- data.frame(json_parsed$trees$nodes)
+    for (v in df[[node_key_name]]) {
+      if (is.na(v)) {
+        next
+      }
+      if (abs(v) > 0.001) {
+        return(TRUE)
+      }
+    }
+    return(FALSE)
+  }
+
+  expect_true(node_exists_with_nonzero("threshold"))
+  expect_true(node_exists_with_nonzero("leaf_value"))
 
   json_parsed$num_feature == 4
   json_parsed$task_type == "kBinaryClfRegr"
